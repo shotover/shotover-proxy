b62bbcf31a8c543a157900d6aa4c11b7c221160a
Ben <ben.bromhead@gmail.com>
2020-12-21 11:09:48 +1300

    upversion

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::net::SocketAddr;
 use tokio::runtime;
 
 #[derive(Clap)]
-#[clap(version = "0.0.17", author = "Instaclustr")]
+#[clap(version = "0.0.18", author = "Instaclustr")]
 struct ConfigOpts {
     #[clap(short, long, default_value = "config/topology.yaml")]
     pub topology_file: String,
