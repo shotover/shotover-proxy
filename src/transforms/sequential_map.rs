9826b3d1584e15d707eb079335c77b121084ddf3
Ben <ben.bromhead@gmail.com>
2021-01-11 23:12:04 +1300

    wip-rewrite

--- src/transforms/sequential_map.rs
+++ src/transforms/sequential_map.rs
@@ -1,18 +1,33 @@
-use crate::config::topology::TopicHolder;
+use crate::config::topology::{ChannelMessage, TopicHolder};
 use crate::error::ChainResponse;
-use crate::message::Messages;
+use crate::message::{Message, Messages, QueryResponse, Value};
+use std::iter::*;
+
+use crate::protocols::RawFrame;
 use crate::transforms::chain::TransformChain;
 use crate::transforms::{
     build_chain_from_config, Transform, Transforms, TransformsConfig, TransformsFromConfig, Wrapper,
 };
-use anyhow::Result;
+use anyhow::{Error, Result};
 use async_trait::async_trait;
+use bytes::Buf;
+use futures::stream::FuturesOrdered;
+use futures::StreamExt;
+use itertools::Itertools;
+use redis_protocol::types::Frame;
 use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, HashMap};
+use tokio::sync::mpsc::Sender;
+use tokio::sync::oneshot::error::RecvError;
+
+const SLOT_SIZE: usize = 16384;
 
 #[derive(Debug, Clone)]
 pub struct SequentialMap {
     name: &'static str,
     chain: TransformChain,
+    pub slots: SlotMap,
+    pub channels: ChannelMap,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
@@ -31,26 +46,179 @@ impl TransformsFromConfig for SequentialMapConfig {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum RoutingInfo {
+    AllNodes,
+    AllMasters,
+    Random,
+    Slot(u16),
+}
+
+fn get_hashtag(key: &[u8]) -> Option<&[u8]> {
+    let open = key.iter().position(|v| *v == b'{');
+    let open = match open {
+        Some(open) => open,
+        None => return None,
+    };
+
+    let close = key[open..].iter().position(|v| *v == b'}');
+    let close = match close {
+        Some(close) => close,
+        None => return None,
+    };
+
+    let rv = &key[open + 1..open + close];
+    if rv.is_empty() {
+        None
+    } else {
+        Some(rv)
+    }
+}
+
+impl RoutingInfo {
+    pub fn for_command_frame(args: &Vec<Frame>) -> Option<RoutingInfo> {
+        if let Some(Frame::BulkString(command_arg)) = args.get(0) {
+            return match command_arg.bytes() {
+                b"FLUSHALL" | b"FLUSHDB" | b"SCRIPT" | b"ACL" => Some(RoutingInfo::AllMasters),
+                b"ECHO" | b"CONFIG" | b"CLIENT" | b"SLOWLOG" | b"DBSIZE" | b"LASTSAVE"
+                | b"PING" | b"INFO" | b"BGREWRITEAOF" | b"BGSAVE" | b"CLIENT LIST" | b"SAVE"
+                | b"TIME" | b"KEYS" | b"AUTH" => Some(RoutingInfo::AllNodes),
+                b"SCAN" | b"CLIENT SETNAME" | b"SHUTDOWN" | b"SLAVEOF" | b"REPLICAOF"
+                | b"SCRIPT KILL" | b"MOVE" | b"BITOP" => None,
+                b"EVALSHA" | b"EVAL" => {
+                    let foo = if let Some(Frame::Integer(key_count)) = args.get(2) {
+                        if *key_count == 0 {
+                            Some(RoutingInfo::Random)
+                        } else {
+                            args.get(3).and_then(RoutingInfo::for_key)
+                        }
+                    } else {
+                        None
+                    };
+                    foo
+                }
+                b"XGROUP" | b"XINFO" => args.get(2).and_then(RoutingInfo::for_key),
+                b"XREAD" | b"XREADGROUP" => {
+                    let streams_position = args.iter().position(|a| match a {
+                        Frame::BulkString(a) => a.bytes() == b"STREAMS",
+                        _ => false,
+                    })?;
+                    args.get(streams_position + 1)
+                        .and_then(RoutingInfo::for_key)
+                }
+                _ => match args.get(1) {
+                    Some(key) => RoutingInfo::for_key(key),
+                    None => Some(RoutingInfo::Random),
+                },
+            };
+        }
+        None
+    }
+
+    pub fn for_key(key: &Frame) -> Option<RoutingInfo> {
+        if let Frame::BulkString(key) = key {
+            let key = match get_hashtag(&key) {
+                Some(tag) => tag,
+                None => &key,
+            };
+            Some(RoutingInfo::Slot(
+                crc16::State::<crc16::XMODEM>::calculate(key) % SLOT_SIZE as u16,
+            ))
+        } else {
+            None
+        }
+    }
+}
+
+type SlotMap = BTreeMap<u16, Sender<ChannelMessage>>;
+type ChannelMap = HashMap<String, Sender<ChannelMessage>>;
+
+fn short_circuit(one_tx: tokio::sync::oneshot::Sender<ChainResponse>) {
+    one_tx.send(Ok(Messages::new_single_response(
+        QueryResponse::empty_with_error(Some(Value::Strings(
+            "ERR Could not route request".to_string(),
+        ))),
+        true,
+        RawFrame::NONE,
+    )));
+}
+
 #[async_trait]
 impl Transform for SequentialMap {
     async fn transform<'a>(&'a mut self, mut qd: Wrapper<'a>) -> ChainResponse {
-        let mut results: Vec<Messages> = Vec::with_capacity(qd.message.messages.len());
-        while !qd.message.messages.is_empty() {
-            results.push(
-                self.chain
-                    .process_request(
-                        Wrapper::new(Messages {
-                            messages: vec![qd.message.messages.remove(0)],
-                        }),
-                        "SequentialMap".to_string(),
-                    )
-                    .await?,
-            )
+        let mut responses = FuturesOrdered::new();
+        for message in qd.message.into_iter() {
+            let sender = match &message.original {
+                RawFrame::Redis(Frame::Array(ref commands)) => {
+                    match RoutingInfo::for_command_frame(&commands) {
+                        Some(RoutingInfo::Slot(slot)) => {
+                            if let Some((_, sender)) = self.slots.range_mut(&slot..).next() {
+                                vec![sender]
+                            } else {
+                                vec![]
+                            }
+                        }
+                        Some(RoutingInfo::AllNodes) | Some(RoutingInfo::AllMasters) => {
+                            self.channels.iter_mut().map(|(_, chan)| chan).collect_vec()
+                        }
+                        Some(RoutingInfo::Random) => {
+                            let key = self
+                                .channels
+                                .keys()
+                                .next()
+                                .unwrap_or(&"nothing".to_string())
+                                .clone();
+                            self.channels.get_mut(&key).into_iter().collect_vec()
+                        }
+                        None => {
+                            vec![]
+                        }
+                    }
+                }
+                _ => {
+                    vec![]
+                }
+            };
+            if sender.is_empty() {
+                let (one_tx, one_rx) = tokio::sync::oneshot::channel::<ChainResponse>();
+                short_circuit(one_tx);
+                responses.push(one_rx)
+            } else {
+                for chan in sender.into_iter() {
+                    let (one_tx, one_rx) = tokio::sync::oneshot::channel::<ChainResponse>();
+                    chan.send(ChannelMessage::new(
+                        Messages::new_from_message(message.clone()),
+                        one_tx,
+                    ))
+                    .await?;
+                    responses.push(one_rx)
+                }
+            }
         }
+        let mut response_buffer = vec![];
+        loop {
+            match responses.next().await {
+                Some(mut r) => {
+                    let mut m = r??.messages.remove(0);
+                    match m.original {
+                        RawFrame::Redis(Frame::Moved(moved_message)) => {
+                            //update slots
+                            //handle request
+                        }
+                        RawFrame::Redis(Frame::Ask(moved_message)) => {
+                            // see redis-protocol.rs for redirection struct so we dont need to parse the string again
+                        }
+                        _ => {}
+                    }
 
-        Ok(Messages {
-            messages: results.into_iter().flat_map(|ms| ms.messages).collect(),
-        })
+                    // response_buffer.append(&mut m.messages);
+                }
+                None => break,
+            }
+        }
+        return Ok(Messages {
+            messages: response_buffer,
+        });
     }
 
     fn get_name(&self) -> &'static str {
