198e652c3f7cb31847d22f562eadb528ecc13d24
Lucas Kent <rubickent@gmail.com>
2021-08-04 15:36:00 +1000

    Refactor Runner to always setup logging and add new method with_observability_interface

--- shotover-proxy/src/runner.rs
+++ shotover-proxy/src/runner.rs
@@ -2,11 +2,16 @@ use std::net::SocketAddr;
 
 use anyhow::{anyhow, Result};
 use clap::{crate_version, Clap};
-use tracing::{debug, info};
+use metrics_runtime::Receiver;
 use tokio::runtime::{self, Runtime};
 use tokio::task::JoinHandle;
-use metrics_runtime::Receiver;
-use tracing_appender::non_blocking::WorkerGuard;
+use tracing::{debug, info};
+use tracing_appender::non_blocking::{WorkerGuard, NonBlocking};
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::fmt::format::{Format, DefaultFields};
+use tracing_subscriber::layer::Layered;
+use tracing_subscriber::reload::Handle;
+use tracing_subscriber::{EnvFilter, Registry};
 
 use crate::config::topology::Topology;
 use crate::config::Config;
@@ -42,11 +47,10 @@ impl Default for ConfigOpts {
 }
 
 pub struct Runner {
-    rt: Runtime,
+    runtime: Runtime,
     topology: Topology,
     config: Config,
-    /// Once this is dropped tracing logs are ignored
-    tracing_guard: Option<WorkerGuard>,
+    tracing: TracingState,
 }
 
 impl Runner {
@@ -54,7 +58,7 @@ impl Runner {
         let config = Config::from_file(params.config_file.clone())?;
         let topology = Topology::from_file(params.topology_file.clone())?;
 
-        let rt = runtime::Builder::new_multi_thread()
+        let runtime = runtime::Builder::new_multi_thread()
             .enable_all()
             .thread_name("RPProxy-Thread")
             .thread_stack_size(params.stack_size)
@@ -62,42 +66,66 @@ impl Runner {
             .build()
             .unwrap();
 
-        let tracing_guard = None;
+        let tracing = TracingState::new(config.main_log_level.as_str());
 
-        Ok(Runner { rt, topology, config, tracing_guard })
+        Ok(Runner { runtime, topology, config, tracing })
     }
 
-    pub fn with_logging(mut self) -> Result<Self> {
-        let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
-        self.tracing_guard = Some(guard);
-
-        let builder = tracing_subscriber::fmt()
-            .with_writer(non_blocking)
-            .with_env_filter(self.config.main_log_level.as_str())
-            .with_filter_reloading();
-        let handle = builder.reload_handle();
-        builder
-            .try_init()
-            .map_err(|e| anyhow!("couldn't start logging - {}", e))?;
-
+    pub fn with_observability_interface(self) -> Result<Self> {
         let receiver = Receiver::builder().build().expect("failed to create receiver");
         let socket: SocketAddr = self.config.observability_interface.parse()?;
-        let exporter = LogFilterHttpExporter::new(receiver.controller(), socket, handle);
+        let exporter = LogFilterHttpExporter::new(receiver.controller(), socket, self.tracing.handle.clone());
 
         receiver.install();
-        self.rt.spawn(exporter.async_run());
+        self.runtime.spawn(exporter.async_run());
 
         Ok(self)
     }
 
-    pub fn run_spawn(self) -> (Runtime, JoinHandle<Result<()>>) {
-        let handle = self.rt.spawn(run(self.topology, self.config));
-        (self.rt, handle)
+    pub fn run_spawn(self) -> RunnerSpawned {
+        let handle = self.runtime.spawn(run(self.topology, self.config));
+
+        RunnerSpawned {
+            runtime: self.runtime,
+            tracing_guard: self.tracing.guard,
+            handle,
+        }
     }
 
     pub fn run_block(self) -> Result<()> {
-        self.rt.block_on(run(self.topology, self.config))
+        self.runtime.block_on(run(self.topology, self.config))
     }
+}
+
+struct TracingState {
+    /// Once this is dropped tracing logs are ignored
+    guard: WorkerGuard,
+    handle: Handle<EnvFilter, Layered<Layer<Registry, DefaultFields, Format, NonBlocking>, Registry>>,
+}
+
+impl TracingState {
+    fn new(log_level: &str) -> Self {
+        let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
+
+        let builder = tracing_subscriber::fmt()
+            .with_writer(non_blocking)
+            .with_env_filter(log_level)
+            .with_filter_reloading();
+        let handle = builder.reload_handle();
+
+        // To avoid unit tests that run in the same excutable from blowing up when they try to reinitialize tracing we ignore the result returned by try_init.
+        // Currently the implementation of try_init will only fail when it is called multiple times.
+        builder.try_init().ok();
+
+        TracingState { guard, handle }
+    }
+}
+
+
+pub struct RunnerSpawned {
+    pub runtime: Runtime,
+    pub handle: JoinHandle<Result<()>>,
+    pub tracing_guard: WorkerGuard,
 }
 
 pub async fn run(topology: Topology, config: Config) -> Result<()> {
--- shotover-proxy/tests/helpers/mod.rs
+++ shotover-proxy/tests/helpers/mod.rs
@@ -2,15 +2,17 @@ use anyhow::Result;
 use shotover_proxy::runner::{ConfigOpts, Runner};
 use tokio::task::JoinHandle;
 use tokio::runtime::Runtime;
-use tracing::Level;
 
 pub fn run_shotover_with_topology(topology_path: &str) -> (Runtime, JoinHandle<Result<()>>) {
-    tracing_subscriber::fmt().with_max_level(Level::INFO).try_init().ok();
-
     let opts = ConfigOpts {
         topology_file: topology_path.into(),
         config_file: "config/config.yaml".into(),
         .. ConfigOpts::default()
     };
-    Runner::new(opts).unwrap().run_spawn()
+    let spawn = Runner::new(opts).unwrap().run_spawn();
+
+    // If we allow the tracing_guard to be dropped then the following tests in the same file will not get tracing so we mem::forget it.
+    // This is because tracing can only be initialized once in the same execution, secondary attempts to initalize tracing will silently fail.
+    std::mem::forget(spawn.tracing_guard);
+    (spawn.runtime, spawn.handle)
 }
