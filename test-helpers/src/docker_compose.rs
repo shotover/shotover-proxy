e033bf716851b03530e1597711822c3b334bded5
Lucas Kent <rubickent@gmail.com>
2021-08-17 13:53:44 +1000

    Tests trigger shotover shutdown and then unwrap the result of the join handle (#139)

--- test-helpers/src/docker_compose.rs
+++ test-helpers/src/docker_compose.rs
@@ -1,23 +1,26 @@
+use anyhow::{anyhow, Result};
 use std::thread;
 use std::time;
 use subprocess::{Exec, Redirection};
 use tracing::info;
 
-fn run_command(command: &str, args: &[&str]) {
+fn run_command(command: &str, args: &[&str]) -> Result<()> {
     let data = Exec::cmd(command)
         .args(args)
         .stdout(Redirection::Pipe)
         .stderr(Redirection::Merge)
-        .capture()
-        .unwrap();
-    if !data.exit_status.success() {
-        panic!(
+        .capture()?;
+
+    if data.exit_status.success() {
+        Ok(())
+    } else {
+        Err(anyhow!(
             "command {} {:?} exited with {:?} and output:\n{}",
             command,
             args,
             data.exit_status,
             data.stdout_str()
-        )
+        ))
     }
 }
 
@@ -27,11 +30,11 @@ pub struct DockerCompose {
 
 impl DockerCompose {
     pub fn new(file_path: &str) -> Self {
-        DockerCompose::clean_up(file_path);
+        DockerCompose::clean_up(file_path).unwrap();
 
         info!("bringing up docker compose {}", file_path);
 
-        run_command("docker-compose", &["-f", file_path, "up", "-d"]);
+        run_command("docker-compose", &["-f", file_path, "up", "-d"]).unwrap();
 
         thread::sleep(time::Duration::from_secs(4));
 
@@ -40,18 +43,29 @@ impl DockerCompose {
         }
     }
 
-    fn clean_up(file_path: &str) {
+    fn clean_up(file_path: &str) -> Result<()> {
         info!("bringing down docker compose {}", file_path);
 
-        run_command("docker-compose", &["-f", file_path, "down", "-v"]);
-        run_command("docker-compose", &["-f", file_path, "rm", "-f", "-s", "-v"]);
+        run_command("docker-compose", &["-f", file_path, "down", "-v"])?;
+        run_command("docker-compose", &["-f", file_path, "rm", "-f", "-s", "-v"])?;
 
         thread::sleep(time::Duration::from_secs(1));
+
+        Ok(())
     }
 }
 
 impl Drop for DockerCompose {
     fn drop(&mut self) {
-        DockerCompose::clean_up(&self.file_path);
+        if std::thread::panicking() {
+            if let Err(err) = DockerCompose::clean_up(&self.file_path) {
+                println!(
+                    "ERROR: docker compose failed to bring down while already panicking: {:?}",
+                    err
+                );
+            }
+        } else {
+            DockerCompose::clean_up(&self.file_path).unwrap();
+        }
     }
 }
