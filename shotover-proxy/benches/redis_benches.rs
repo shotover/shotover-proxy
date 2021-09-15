c97e2ffc1a05910dda43086bfdc25836ea946bd7
Claude Warren <claude.warren@instaclustr.com>
2021-09-15 07:05:26 +0100

    Revert "Test timing"

--- shotover-proxy/benches/redis_benches.rs
+++ shotover-proxy/benches/redis_benches.rs
@@ -7,7 +7,6 @@ use helpers::ShotoverManager;
 
 fn redis_active_bench(c: &mut Criterion) {
     let _compose = DockerCompose::new("examples/redis-multi/docker-compose.yml");
-    _compose.wait_for("Cluster correctly created").unwrap();
     let shotover_manager =
         ShotoverManager::from_topology_file("examples/redis-multi/topology.yaml");
 
@@ -26,7 +25,6 @@ fn redis_active_bench(c: &mut Criterion) {
 
 fn redis_cluster_bench(c: &mut Criterion) {
     let _compose = DockerCompose::new("examples/redis-cluster/docker-compose.yml");
-    _compose.wait_for("Cluster correctly created").unwrap();
     let shotover_manager =
         ShotoverManager::from_topology_file("examples/redis-cluster/topology.yaml");
 
@@ -45,7 +43,6 @@ fn redis_cluster_bench(c: &mut Criterion) {
 
 fn redis_passthrough_bench(c: &mut Criterion) {
     let _compose = DockerCompose::new("examples/redis-passthrough/docker-compose.yml");
-    _compose.wait_for("Cluster correctly created").unwrap();
     let shotover_manager =
         ShotoverManager::from_topology_file("examples/redis-passthrough/topology.yaml");
 
