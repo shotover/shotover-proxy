d3c55841fe89d549a2c1208d541d76e466fa6cab
conorbros <conor.brosnan@instaclustr.com>
2021-12-17 13:27:33 +1000

    int sizes

--- shotover-proxy/src/protocols/cassandra_codec.rs
+++ shotover-proxy/src/protocols/cassandra_codec.rs
@@ -33,7 +33,8 @@ use tokio_util::codec::{Decoder, Encoder};
 use tracing::{debug, error, info, warn};
 
 use crate::message::{
-    ASTHolder, Message, MessageDetails, Messages, QueryMessage, QueryResponse, QueryType, Value,
+    ASTHolder, IntSize, Message, MessageDetails, Messages, QueryMessage, QueryResponse, QueryType,
+    Value,
 };
 use crate::protocols::RawFrame;
 
@@ -145,9 +146,24 @@ impl CassandraCodec {
                                         Value::NULL => to_int(-1_i32),
                                         Value::Bytes(x) => x.to_vec(),
                                         Value::Strings(x) => Vec::from(x.as_bytes()),
-                                        Value::Integer(x, _) => {
+                                        Value::Integer(x, size) => {
                                             let mut temp: Vec<u8> = Vec::new();
-                                            let _ = temp.write_i64::<BigEndian>(*x).unwrap();
+
+                                            match size {
+                                                IntSize::I64 => {
+                                                    temp.write_i64::<BigEndian>(*x).unwrap();
+                                                }
+                                                IntSize::I32 => {
+                                                    temp.write_i32::<BigEndian>(*x as i32).unwrap();
+                                                }
+                                                IntSize::I16 => {
+                                                    temp.write_i16::<BigEndian>(*x as i16).unwrap();
+                                                }
+                                                IntSize::I8 => {
+                                                    temp.write_i8(*x as i8).unwrap();
+                                                }
+                                            }
+
                                             temp
                                         }
                                         Value::Float(x) => {
