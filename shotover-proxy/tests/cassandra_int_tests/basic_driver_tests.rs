b780b1d5443c04a71f19b55905d20c4aab2c444a
conorbros <conor.brosnan@instaclustr.com>
2022-02-01 11:58:41 +1000

    add insert and delete tests

--- shotover-proxy/tests/cassandra_int_tests/basic_driver_tests.rs
+++ shotover-proxy/tests/cassandra_int_tests/basic_driver_tests.rs
@@ -1,6 +1,5 @@
-use crate::cassandra_int_tests::{assert_rows, cassandra_connection, run_query, ResultValue};
+use crate::cassandra_int_tests::cassandra_connection;
 use crate::helpers::ShotoverManager;
-use cassandra_cpp::Session;
 use serial_test::serial;
 use test_helpers::docker_compose::DockerCompose;
 
@@ -977,47 +976,90 @@ APPLY BATCH;"#,
     }
 }
 
-fn prepared_statements(session: &Session) {
-    run_query(session, "CREATE KEYSPACE test_prepare_statements WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
-    run_query(
-        session,
-        "CREATE TABLE test_prepare_statements.table_1 (id int PRIMARY KEY, x int, name varchar);",
-    );
+mod prepared_statements {
+    use crate::cassandra_int_tests::{assert_query_result, assert_rows, run_query, ResultValue};
+    use cassandra_cpp::Session;
 
-    run_query(
-        session,
-        r#"BEGIN BATCH
-                INSERT INTO test_prepare_statements.table_1 (id, x, name) VALUES (1, 11, 'foo');
-                INSERT INTO test_prepare_statements.table_1 (id, x, name) VALUES (2, 12, 'bar');
-                INSERT INTO test_prepare_statements.table_1 (id, x, name) VALUES (3, 13, 'baz');
-            APPLY BATCH;"#,
-    );
+    fn delete(session: &Session) {
+        let prepared = session
+            .prepare("DELETE FROM test_prepare_statements.table_1 WHERE id = ?;")
+            .unwrap()
+            .wait()
+            .unwrap();
 
-    let prepared = session
-        .prepare("SELECT id, x, name FROM test_prepare_statements.table_1 WHERE id = ?")
-        .unwrap()
-        .wait()
-        .unwrap();
+        let mut statement = prepared.bind();
+        statement.bind_int32(0, 1).unwrap();
+        session.execute(&statement).wait().unwrap();
 
-    let mut statement = prepared.bind();
-    statement.bind_int32(0, 1).unwrap();
+        assert_query_result(
+            session,
+            "SELECT * FROM test_prepare_statements.table_1 where id = 1;",
+            &[],
+        );
+    }
 
-    let result_rows = session
-        .execute(&statement)
-        .wait()
-        .unwrap()
-        .into_iter()
-        .map(|x| x.into_iter().map(ResultValue::new).collect())
-        .collect();
+    fn insert(session: &Session) {
+        let prepared = session
+            .prepare("INSERT INTO test_prepare_statements.table_1 (id, x, name) VALUES (?, ?, ?);")
+            .unwrap()
+            .wait()
+            .unwrap();
 
-    assert_rows(
-        result_rows,
-        &[&[
-            ResultValue::Int(1),
-            ResultValue::Int(11),
-            ResultValue::Varchar("foo".into()),
-        ]],
-    );
+        let mut statement = prepared.bind();
+        statement.bind_int32(0, 1).unwrap();
+        statement.bind_int32(1, 11).unwrap();
+        statement.bind_string(2, "foo").unwrap();
+        session.execute(&statement).wait().unwrap();
+
+        statement = prepared.bind();
+        statement.bind_int32(0, 2).unwrap();
+        statement.bind_int32(1, 12).unwrap();
+        statement.bind_string(2, "bar").unwrap();
+        session.execute(&statement).wait().unwrap();
+
+        statement = prepared.bind();
+        statement.bind_int32(0, 2).unwrap();
+        statement.bind_int32(1, 13).unwrap();
+        statement.bind_string(2, "baz").unwrap();
+        session.execute(&statement).wait().unwrap();
+    }
+
+    fn select(session: &Session) {
+        let prepared = session
+            .prepare("SELECT id, x, name FROM test_prepare_statements.table_1 WHERE id = ?")
+            .unwrap()
+            .wait()
+            .unwrap();
+
+        let mut statement = prepared.bind();
+        statement.bind_int32(0, 1).unwrap();
+
+        let result_rows = session
+            .execute(&statement)
+            .wait()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.into_iter().map(ResultValue::new).collect())
+            .collect();
+
+        assert_rows(
+            result_rows,
+            &[&[
+                ResultValue::Int(1),
+                ResultValue::Int(11),
+                ResultValue::Varchar("foo".into()),
+            ]],
+        );
+    }
+
+    pub fn test(session: &Session) {
+        run_query(session, "CREATE KEYSPACE test_prepare_statements WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
+        run_query(session, "CREATE TABLE test_prepare_statements.table_1 (id int PRIMARY KEY, x int, name varchar);");
+
+        insert(session);
+        select(session);
+        delete(session);
+    }
 }
 
 mod cache {
@@ -1199,6 +1241,7 @@ fn test_cluster() {
     native_types::test(&connection);
     collections::test(&connection);
     functions::test(&connection);
+    prepared_statements::test(&connection);
 }
 
 #[test]
@@ -1218,7 +1261,7 @@ fn test_passthrough() {
     native_types::test(&connection);
     collections::test(&connection);
     functions::test(&connection);
-    prepared_statements(&connection);
+    prepared_statements::test(&connection);
 }
 
 #[test]
@@ -1238,5 +1281,5 @@ fn test_cassandra_redis_cache() {
     udt::test(&connection);
     functions::test(&connection);
     cache::test(&connection, &mut redis_connection);
-    prepared_statements(&connection);
+    prepared_statements::test(&connection);
 }
