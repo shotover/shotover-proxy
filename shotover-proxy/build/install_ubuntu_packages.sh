8887714b6cd92b1a9d72a5581c462acb2fac88c8
Lucas Kent <rubickent@gmail.com>
2022-03-15 17:27:21 +1100

    Add check for untracked/uncommitted files in CI

--- shotover-proxy/build/install_ubuntu_packages.sh
+++ shotover-proxy/build/install_ubuntu_packages.sh
@@ -9,6 +9,8 @@ sudo apt-get install -y libpcap-dev wget
 
 if [ ${DISTRIB_RELEASE} == "18.04" ]
 then
+  mkdir pkgs
+  pushd pkgs
   wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/cassandra/v2.16.0/cassandra-cpp-driver_2.16.0-1_amd64.deb &
   wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/cassandra/v2.16.0/cassandra-cpp-driver-dev_2.16.0-1_amd64.deb &
   wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/dependencies/libuv/v1.35.0/libuv1_1.35.0-1_amd64.deb &
@@ -18,5 +20,8 @@ then
   sudo apt -y install ./cassandra-cpp-driver_2.16.0-1_amd64.deb \
     ./cassandra-cpp-driver-dev_2.16.0-1_amd64.deb \
     ./libuv1_1.35.0-1_amd64.deb ./libuv1-dev_1.35.0-1_amd64.deb
+
+  popd
+  rm -r pkgs
 fi
 
