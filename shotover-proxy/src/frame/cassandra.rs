6454c03d70e6c10b61fc0e3195250f40f95fc2c0
conorbros <conor.brosnan@instaclustr.com>
2022-03-21 11:17:47 +1000

    review feedback

--- shotover-proxy/src/frame/cassandra.rs
+++ shotover-proxy/src/frame/cassandra.rs
@@ -36,8 +36,8 @@ fn get_batch_len(bytes: &[u8]) -> Result<NonZeroU32> {
     }
 
     let short_bytes = &bytes[1..3];
-    let short = u16::from_be_bytes(short_bytes.try_into()?).max(1); // it is valid for a batch statement to have 0 statements, but for the purposes of shotover throttling we can count it as one query
-    Ok(NonZeroU32::new(short.into()).unwrap())
+    let short = u16::from_be_bytes(short_bytes.try_into()?); // it is valid for a batch statement to have 0 statements, but for the purposes of shotover throttling we can count it as one query
+    Ok(NonZeroU32::new(short.into()).unwrap_or(nonzero!(1u32)))
 }
 
 pub(crate) struct CassandraMetadata {
