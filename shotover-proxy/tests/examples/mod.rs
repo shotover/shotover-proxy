1e856e9dfb2cfcbe5a754f889a5ad354b578e0db
conorbros <conor.brosnan@instaclustr.com>
2022-03-23 18:39:13 +1000

    move cassandra_connection to a function

--- shotover-proxy/tests/examples/mod.rs
+++ shotover-proxy/tests/examples/mod.rs
@@ -1,5 +1,4 @@
-use crate::cassandra_int_tests::{assert_query_result, ResultValue};
-use crate::helpers::ShotoverManager;
+use crate::cassandra_int_tests::{assert_query_result, cassandra_connection, ResultValue};
 use serial_test::serial;
 use test_helpers::docker_compose::DockerCompose;
 
@@ -9,11 +8,7 @@ async fn test_cassandra_rewrite_peers_example() {
     let _docker_compose =
         DockerCompose::new("example-configs/cassandra-rewrite-peers/docker-compose.yml");
 
-    let shotover_manager = ShotoverManager::from_topology_file(
-        "example-configs/cassandra-rewrite-peers/topology.yaml",
-    );
-
-    let connection = shotover_manager.cassandra_connection("172.16.1.2", 9043);
+    let connection = cassandra_connection("172.16.1.2", 9043);
 
     assert_query_result(
         &connection,
