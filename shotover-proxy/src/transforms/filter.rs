9abcee868cb2bb69954bd96d9534bb0e491adcc8
Lucas Kent <rubickent@gmail.com>
2022-04-04 13:33:12 +1000

    Store Wrapper.transforms as an IterMut instead of a cloned Vec

--- shotover-proxy/src/transforms/filter.rs
+++ shotover-proxy/src/transforms/filter.rs
@@ -71,7 +71,7 @@ mod test {
             filter: QueryType::Read,
         };
 
-        let mut loopback = Transforms::Loopback(Loopback::default());
+        let mut chain = vec![Transforms::Loopback(Loopback::default())];
 
         let messages: Vec<_> = (0..26)
             .map(|i| {
@@ -91,7 +91,7 @@ mod test {
             .collect();
 
         let mut message_wrapper = Wrapper::new(messages);
-        message_wrapper.transforms = vec![&mut loopback];
+        message_wrapper.reset(&mut chain);
         let result = filter_transform.transform(message_wrapper).await.unwrap();
 
         assert_eq!(result.len(), 26);
