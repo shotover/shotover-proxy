effd59a933737338b4f1e6311f6d5fcf5864dd3d
Lucas Kent <rubickent@gmail.com>
2022-04-12 16:01:53 +1000

    Simplify eval routing

--- shotover-proxy/src/transforms/redis/sink_cluster.rs
+++ shotover-proxy/src/transforms/redis/sink_cluster.rs
@@ -555,26 +555,20 @@ impl RoutingInfo {
             | b"KEYS" | b"ACL" => Some(RoutingInfo::AllNodes),
             b"SCAN" | b"CLIENT SETNAME" | b"SHUTDOWN" | b"SLAVEOF" | b"REPLICAOF"
             | b"SCRIPT KILL" | b"MOVE" | b"BITOP" => None,
-            b"EVALSHA" | b"EVAL" => {
-                //TODO: Appears the the codec is not decoding integers correctly
-                match args.get(2) {
-                    Some(RedisFrame::Integer(key_count)) => Some(*key_count),
-                    Some(RedisFrame::BulkString(key_count)) => {
-                        String::from_utf8(key_count.to_vec())
-                            .unwrap_or_else(|_| "0".to_string())
-                            .parse::<i64>()
-                            .ok()
-                    }
-                    _ => None,
-                }
-                .and_then(|key_count| {
-                    if key_count == 0 {
-                        Some(RoutingInfo::Random)
-                    } else {
-                        args.get(3).and_then(RoutingInfo::for_key)
-                    }
-                })
+            b"EVALSHA" | b"EVAL" => match args.get(2) {
+                Some(RedisFrame::BulkString(key_count)) => std::str::from_utf8(key_count)
+                    .unwrap_or("0")
+                    .parse::<i64>()
+                    .ok(),
+                _ => None,
             }
+            .and_then(|key_count| {
+                if key_count == 0 {
+                    Some(RoutingInfo::Random)
+                } else {
+                    args.get(3).and_then(RoutingInfo::for_key)
+                }
+            }),
             b"XGROUP" | b"XINFO" => args.get(2).and_then(RoutingInfo::for_key),
             b"XREAD" | b"XREADGROUP" => args
                 .iter()
