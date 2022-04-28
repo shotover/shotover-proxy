4ed6ff798f749de05334f2fee8a89a553276f524
Claude Warren <claude.warren@instaclustr.com>
2022-04-28 07:18:31 +0100

    changes as per Lucas

--- shotover-proxy/tests/helpers/cassandra.rs
+++ shotover-proxy/tests/helpers/cassandra.rs
@@ -12,10 +12,9 @@ pub fn cassandra_connection(contact_points: &str, port: u16) -> Session {
     cluster.set_port(port).ok();
     cluster.set_load_balance_round_robin();
     let result = cluster.connect();
-    if let Some(err) = &result.as_ref().err() {
-        panic!("{}", err);
-    }
-    result.unwrap()
+    // By default unwrap uses the Debug formatter `{:?}` which is extremely noisy for the error type returned by `connect()`.
+    // So we instead force the Display formatter `{}` on the error.
+    cluster.connect().map_err(|err| format!("{err}")).unwrap()
 }
 
 #[derive(Debug, Clone, PartialOrd, PartialEq, Eq, Ord)]
