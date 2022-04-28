39fd381f33cab171dadb934b253e463ab540be01
conorbros <conor.brosnan@instaclustr.com>
2022-04-28 11:00:43 +1000

    complete + unit tests

--- shotover-proxy/src/transforms/cassandra/peers_rewrite.rs
+++ shotover-proxy/src/transforms/cassandra/peers_rewrite.rs
@@ -48,10 +48,23 @@ impl Transform for CassandraPeersRewrite {
 }
 
 fn is_system_peers(message: &mut Message) -> bool {
+    let keyspace = "system";
+    let table = "peers_v2";
+    let column = "native_port";
+
+    // If the message isn't parsed only seek the table and column it for better performance
+    if !message.is_parsed() {
+        tracing::info!("using seek function");
+        let bytes = message.get_raw_bytes();
+        return seek_frame::is_reading_table_and_column(bytes, keyspace, table, column);
+    }
+
+    tracing::info!("using frame method");
+    // otherwise if it's parsed we can just use frame
     if let Some(Frame::Cassandra(_)) = message.frame() {
         if let Some(namespace) = message.namespace() {
             if namespace.len() > 1 {
-                return namespace[0] == "system" && namespace[1] == "peers_v2";
+                return namespace[0] == keyspace && namespace[1] == table;
             }
         }
     }
@@ -88,89 +101,228 @@ fn rewrite_port(message: &mut Message, new_port: u32) {
     }
 }
 
+mod seek_frame {
+    use cassandra_protocol::frame::Opcode;
+    use cassandra_protocol::types::INT_LEN;
+    use cql3_parser::{
+        cassandra_ast::CassandraParser,
+        select::{Named, SelectElement},
+    };
+    use std::io::{Cursor, Read};
+
+    pub fn is_reading_table_and_column(
+        bytes: &[u8],
+        keyspace: &str,
+        table: &str,
+        column: &str,
+    ) -> bool {
+        let opcode = Opcode::try_from(bytes[4]).unwrap();
+        match opcode {
+            Opcode::Query => {
+                let mut body_cursor = Cursor::new(bytes);
+                body_cursor.set_position(9);
+                let query = {
+                    let mut buff = [0; INT_LEN];
+                    body_cursor.read_exact(&mut buff).unwrap();
+
+                    let len = i32::from_be_bytes(buff) as usize;
+                    let body_bytes = {
+                        let start = body_cursor.position() as usize;
+                        let result = &body_cursor.get_ref()[start..start + len];
+                        body_cursor.set_position(body_cursor.position() + len as u64);
+
+                        if result.len() != len {
+                            panic!("cursor_next_value_ref could not retrieve a full slice")
+                        } else {
+                            result
+                        }
+                    };
+
+                    std::str::from_utf8(body_bytes).unwrap()
+                };
+
+                let result = check_query(query, keyspace, table, column);
+                tracing::info!("parsed query: {:?}; result: {:?}", query, result);
+                result
+            }
+            _ => false,
+        }
+    }
+
+    fn check_query(
+        query: &str,
+        search_keyspace: &str,
+        search_table: &str,
+        search_column: &str,
+    ) -> bool {
+        let language = tree_sitter_cql::language();
+        let mut parser = tree_sitter::Parser::new();
+        if parser.set_language(language).is_err() {
+            panic!("language version mismatch");
+        }
+
+        let tree = parser.parse(query, None).unwrap();
+
+        let mut cursor = tree.root_node().walk();
+        let _ = cursor.goto_first_child();
+
+        if cursor.node().kind() != "select_statement" {
+            return false;
+        }
+        // skip past "SELECT" node
+        cursor.goto_first_child();
+
+        // Check if these nodes exist and skip past them
+        cursor.goto_next_sibling();
+        if cursor.node().kind().eq("DISTINCT") {
+            cursor.goto_next_sibling();
+        }
+
+        if cursor.node().kind().eq("JSON") {
+            cursor.goto_next_sibling();
+        }
+
+        // skip parsing the columns until we know the table
+        let columns_start_node = {
+            let mut cursor = cursor.node().walk();
+
+            // record where the columns start so we can return
+            let start_node = cursor.node();
+
+            let mut process = cursor.goto_first_child();
+
+            while process {
+                match cursor.node().kind() {
+                    "select_element" => {}
+                    "*" => {}
+                    _ => {}
+                }
+                process = cursor.goto_next_sibling();
+            }
+            start_node
+        };
+
+        let (keyspace, table) = {
+            cursor.goto_next_sibling();
+            let table = CassandraParser::parse_from_spec(&cursor.node(), query);
+            (table.keyspace, table.name)
+        };
+
+        // check keyspace and table
+        if Some(search_keyspace.to_string()) == keyspace && search_table == table {
+            // parse columns now
+            let mut result = vec![];
+            cursor.reset(columns_start_node);
+            cursor.node().walk();
+            let mut process = cursor.goto_first_child();
+
+            while process {
+                match cursor.node().kind() {
+                    "select_element" => {
+                        result.push(CassandraParser::parse_select_element(&cursor.node(), query))
+                    }
+                    "*" => result.push(SelectElement::Star),
+                    _ => {}
+                }
+                process = cursor.goto_next_sibling();
+            }
+
+            // check if the columns contain the column we are looking for or a "*"
+            for x in result {
+                match x {
+                    SelectElement::Column(Named { name, .. }) => {
+                        if search_column == name {
+                            return true;
+                        }
+                    }
+                    SelectElement::Star => {
+                        return true;
+                    }
+                    _ => {
+                        return false;
+                    }
+                };
+            }
+        };
+        false
+    }
+}
+
 #[cfg(test)]
-mod test {
+mod test_seek_frame {
     use super::*;
-    use crate::frame::{CassandraFrame, CQL};
+    use cassandra_protocol::compression::Compression;
+
+    fn get_cassandra_bytes(query: &str) -> Vec<u8> {
+        let original = test_peers_rewrite::create_cassandra_frame(query);
+        original.encode().encode_with(Compression::None).unwrap()
+    }
+
+    fn call_function(query: &str) -> bool {
+        seek_frame::is_reading_table_and_column(
+            &get_cassandra_bytes(query),
+            "system",
+            "peers_v2",
+            "native_port",
+        )
+    }
+
+    #[test]
+    fn test_positive_cases() {
+        assert!(call_function("SELECT * FROM system.peers_v2"));
+
+        assert!(call_function(
+            "SELECT data_center, native_port, rack FROM system.peers_v2;"
+        ));
+
+        assert!(call_function("SELECT native_port FROM system.peers_v2;"));
+        assert!(call_function(
+            "SELECT native_port, data_center, rack FROM system.peers_v2;"
+        ));
+    }
+
+    #[test]
+    fn test_negative_cases() {
+        assert!(!call_function(
+            "SELECT native_port FROM not_system.peers_v2"
+        ));
+
+        assert!(!call_function(
+            "SELECT native_port FROM system.not_peers_v2"
+        ));
+
+        assert!(!call_function(
+            "SELECT not_native_port FROM system.peers_v2"
+        ));
+    }
+}
+
+#[cfg(test)]
+mod test_peers_rewrite {
+    use super::*;
+    use crate::frame::{CassandraFrame, Frame, CQL};
     use crate::transforms::cassandra::peers_rewrite::CassandraResult::Rows;
     use cassandra_protocol::{
         consistency::Consistency,
         frame::{
-            frame_result::{
-                ColSpec,
-                ColType::{Inet, Int},
-                ColTypeOption, RowsMetadata, RowsMetadataFlags, TableSpec,
-            },
+            frame_result::ColType::{Inet, Int},
+            frame_result::{ColSpec, ColTypeOption, RowsMetadata, RowsMetadataFlags, TableSpec},
             Version,
         },
         query::QueryParams,
     };
 
-    fn create_query_message(query: String) -> Message {
-        let original = Frame::Cassandra(CassandraFrame {
-            version: Version::V4,
-            stream_id: 0,
-            tracing_id: None,
-            warnings: vec![],
-            operation: CassandraOperation::Query {
-                query: CQL::parse_from_string(query),
-                params: QueryParams {
-                    consistency: Consistency::One,
-                    with_names: false,
-                    values: None,
-                    page_size: Some(5000),
-                    paging_state: None,
-                    serial_consistency: None,
-                    timestamp: Some(1643855761086585),
-                },
-            },
-        });
-
-        Message::from_frame(original)
-    }
-
-    fn create_response_message(rows: Vec<Vec<MessageValue>>) -> Message {
-        let original = Frame::Cassandra(CassandraFrame {
-            version: Version::V4,
-            stream_id: 0,
-            tracing_id: None,
-            warnings: vec![],
-            operation: CassandraOperation::Result(Rows {
-                value: MessageValue::Rows(rows),
-                metadata: RowsMetadata {
-                    flags: RowsMetadataFlags::GLOBAL_TABLE_SPACE,
-                    columns_count: 1,
-                    paging_state: None,
-                    global_table_spec: Some(TableSpec {
-                        ks_name: "system".into(),
-                        table_name: "peers_v2".into(),
-                    }),
-                    col_specs: vec![ColSpec {
-                        table_spec: None,
-                        name: "native_port".into(),
-                        col_type: ColTypeOption {
-                            id: Int,
-                            value: None,
-                        },
-                    }],
-                },
-            }),
-        });
-
-        Message::from_frame(original)
-    }
-
     #[test]
     fn test_is_system_peers_v2() {
         assert!(is_system_peers(&mut create_query_message(
-            "SELECT * FROM system.peers_v2;".into()
+            "SELECT * FROM system.peers_v2;"
         )));
 
         assert!(!is_system_peers(&mut create_query_message(
-            "SELECT * FROM not_system.peers_v2;".into()
+            "SELECT * FROM not_system.peers_v2;"
         )));
 
-        assert!(!is_system_peers(&mut create_query_message("".into())));
+        assert!(!is_system_peers(&mut create_query_message("")));
     }
 
     #[test]
@@ -231,5 +383,62 @@ mod test {
 
             assert_eq!(original, expected);
         }
+    }
+
+    pub fn create_query_message(query: &str) -> Message {
+        let original = Frame::Cassandra(create_cassandra_frame(query));
+        Message::from_frame(original)
+    }
+
+    pub fn create_cassandra_frame(query: &str) -> CassandraFrame {
+        CassandraFrame {
+            version: Version::V4,
+            stream_id: 0,
+            tracing_id: None,
+            warnings: vec![],
+            operation: CassandraOperation::Query {
+                query: CQL::parse_from_string(query.to_string()),
+                params: QueryParams {
+                    consistency: Consistency::One,
+                    with_names: false,
+                    values: None,
+                    page_size: Some(5000),
+                    paging_state: None,
+                    serial_consistency: None,
+                    timestamp: Some(1643855761086585),
+                },
+            },
+        }
+    }
+
+    pub fn create_response_message(rows: Vec<Vec<MessageValue>>) -> Message {
+        let original = Frame::Cassandra(CassandraFrame {
+            version: Version::V4,
+            stream_id: 0,
+            tracing_id: None,
+            warnings: vec![],
+            operation: CassandraOperation::Result(Rows {
+                value: MessageValue::Rows(rows),
+                metadata: RowsMetadata {
+                    flags: RowsMetadataFlags::GLOBAL_TABLE_SPACE,
+                    columns_count: 1,
+                    paging_state: None,
+                    global_table_spec: Some(TableSpec {
+                        ks_name: "system".into(),
+                        table_name: "peers_v2".into(),
+                    }),
+                    col_specs: vec![ColSpec {
+                        table_spec: None,
+                        name: "native_port".into(),
+                        col_type: ColTypeOption {
+                            id: Int,
+                            value: None,
+                        },
+                    }],
+                },
+            }),
+        });
+
+        Message::from_frame(original)
     }
 }
