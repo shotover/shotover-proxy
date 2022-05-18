fd47e70ec5ca0b843f73b2b0478c960fb43d3bd8
Conor <conor.brosnan@instaclustr.com>
2022-05-18 14:18:32 +1000

    temp ci fix (#648)

--- shotover-proxy/build/install_ubuntu_packages.sh
+++ shotover-proxy/build/install_ubuntu_packages.sh
@@ -11,10 +11,10 @@ if [ ${DISTRIB_RELEASE} == "18.04" ]
 then
   mkdir pkgs
   pushd pkgs
-  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/cassandra/v2.16.0/cassandra-cpp-driver_2.16.0-1_amd64.deb &
-  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/cassandra/v2.16.0/cassandra-cpp-driver-dev_2.16.0-1_amd64.deb &
-  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/dependencies/libuv/v1.35.0/libuv1_1.35.0-1_amd64.deb &
-  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/dependencies/libuv/v1.35.0/libuv1-dev_1.35.0-1_amd64.deb &
+  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/cassandra/v2.16.0/cassandra-cpp-driver_2.16.0-1_amd64.deb --no-check-certificate &
+  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/cassandra/v2.16.0/cassandra-cpp-driver-dev_2.16.0-1_amd64.deb --no-check-certificate &
+  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/dependencies/libuv/v1.35.0/libuv1_1.35.0-1_amd64.deb --no-check-certificate &
+  wget https://downloads.datastax.com/cpp-driver/ubuntu/18.04/dependencies/libuv/v1.35.0/libuv1-dev_1.35.0-1_amd64.deb --no-check-certificate &
   wait
 
   sudo apt -y install ./cassandra-cpp-driver_2.16.0-1_amd64.deb \
