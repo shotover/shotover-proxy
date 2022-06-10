6f002236cebf9f9b408964af841cd762dd4578cf
conorbros <conor.brosnan@instaclustr.com>
2022-06-10 17:10:08 +1000

    fix test

--- shotover-proxy/tests/cassandra_int_tests/mod.rs
+++ shotover-proxy/tests/cassandra_int_tests/mod.rs
@@ -12,8 +12,7 @@ use rand::Rng;
 use serial_test::serial;
 use std::sync::Arc;
 use test_helpers::docker_compose::{run_command, DockerCompose};
-use tokio::time::timeout;
-use tokio::time::Duration;
+use tokio::time::{sleep, timeout, Duration};
 
 mod batch_statements;
 mod cache;
@@ -386,32 +385,24 @@ async fn test_events_keyspace() {
     let session = TcpSessionBuilder::new(RoundRobinLoadBalancingStrategy::new(), config).build();
     let mut event_recv = session.create_event_receiver();
 
-    let mut tries = 0;
-    loop {
-        if tries > 3 {
-            panic!("did not receive the event after 3 tries.");
+    sleep(Duration::from_secs(3)).await; // let the driver finish connecting to the cluster and registering for the events
+
+    let n: u8 = rng.gen();
+    let create_ks = format!("CREATE KEYSPACE IF NOT EXISTS test_ks_{} WITH REPLICATION = {{ 'class' : 'SimpleStrategy', 'replication_factor' : 1 }};", n);
+
+    session
+        .query(create_ks)
+        .await
+        .expect("Keyspace creation error");
+
+    match timeout(Duration::from_secs(10), event_recv.recv()).await {
+        Ok(recvd) => {
+            if let Ok(event) = recvd {
+                assert!(matches!(event, ServerEvent::SchemaChange { .. }));
+            };
         }
-
-        let n: u8 = rng.gen();
-        let create_ks = format!("CREATE KEYSPACE IF NOT EXISTS test_ks_{} WITH REPLICATION = {{ 'class' : 'SimpleStrategy', 'replication_factor' : 1 }};", n);
-
-        session
-            .query(create_ks)
-            .await
-            .expect("Keyspace creation error");
-
-        match timeout(Duration::from_secs(10), event_recv.recv()).await {
-            Ok(recvd) => {
-                if let Ok(event) = recvd {
-                    println!("{:?}", event);
-                    assert!(matches!(event, ServerEvent::SchemaChange { .. }));
-                    break;
-                };
-            }
-            Err(err) => {
-                println!("plus tries {}", err);
-                tries += 1;
-            }
+        Err(err) => {
+            panic!("{}", err);
         }
     }
 }
@@ -441,26 +432,20 @@ async fn test_events_node() {
 
     let mut event_recv = session.create_event_receiver();
 
-    run_command("docker", &["kill", "cassandra-three"]).unwrap();
+    sleep(Duration::from_secs(3)).await; // let the driver finish connecting to the cluster and registering for the events
 
-    let mut tries = 0;
-    loop {
-        if tries > 3 {
-            panic!("did not receive the event after 3 tries.");
+    run_command("docker", &["kill", "cassandra-two"]).unwrap();
+
+    match timeout(Duration::from_secs(20), event_recv.recv()).await {
+        Ok(recvd) => {
+            if let Ok(ServerEvent::StatusChange(StatusChange { addr, .. })) = recvd {
+                assert_eq!(addr.addr.port(), 9044);
+            } else {
+                panic!("expected ServerEvent::StatusChange, got {:?}", recvd);
+            }
         }
-
-        match timeout(Duration::from_secs(10), event_recv.recv()).await {
-            Ok(recvd) => {
-                if let Ok(ServerEvent::StatusChange(StatusChange { addr, .. })) = recvd {
-                    assert_eq!(addr.addr.port(), 9044);
-                } else {
-                    panic!("expected ServerEvent::StatusChange, got {:?}", recvd);
-                }
-                break;
-            }
-            Err(_) => {
-                tries += 1;
-            }
+        Err(err) => {
+            panic!("{}", err)
         }
     }
 }
