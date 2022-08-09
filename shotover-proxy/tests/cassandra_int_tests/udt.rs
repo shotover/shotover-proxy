0d5ec995dcacef5eaec7a39a1e2f553dc22697af
Lucas Kent <rubickent@gmail.com>
2022-08-09 16:21:24 +1000

    Fix another intermittent cluster failure

--- shotover-proxy/tests/cassandra_int_tests/udt.rs
+++ shotover-proxy/tests/cassandra_int_tests/udt.rs
@@ -1,3 +1,4 @@
+use crate::cassandra_int_tests::schema_awaiter::SchemaAwaiter;
 use crate::helpers::cassandra::{run_query, CassandraConnection};
 
 fn test_create_udt(session: &CassandraConnection) {
@@ -15,19 +16,21 @@ fn test_create_udt(session: &CassandraConnection) {
     );
 }
 
-fn test_drop_udt(session: &CassandraConnection) {
+async fn test_drop_udt(session: &CassandraConnection, schema_awaiter: &SchemaAwaiter) {
     run_query(
         session,
         "CREATE TYPE test_udt_keyspace.test_type_drop_me (foo text, bar int)",
     );
+    schema_awaiter.await_schema_agreement().await;
     run_query(session, "DROP TYPE test_udt_keyspace.test_type_drop_me;");
+    schema_awaiter.await_schema_agreement().await;
     let statement = "CREATE TABLE test_udt_keyspace.test_delete_table (id int PRIMARY KEY, foo test_type_drop_me);";
     let result = session.execute_expect_err(statement).to_string();
     assert_eq!(result, "Cassandra detailed error SERVER_INVALID_QUERY: Unknown type test_udt_keyspace.test_type_drop_me");
 }
 
-pub fn test(session: &CassandraConnection) {
+pub async fn test(session: &CassandraConnection, schema_awaiter: &SchemaAwaiter) {
     run_query(session, "CREATE KEYSPACE test_udt_keyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
     test_create_udt(session);
-    test_drop_udt(session);
+    test_drop_udt(session, schema_awaiter).await;
 }
