04a744e34c63cc9c2c035d508c481b367dd114ca
Lucas Kent <rubickent@gmail.com>
2022-09-07 09:11:19 +1000

    Upgrade remaining cassandra int tests to cassandra 4.0.6 (#785)

--- shotover-proxy/tests/cassandra_int_tests/cluster_multi_rack.rs
+++ shotover-proxy/tests/cassandra_int_tests/cluster_multi_rack.rs
@@ -12,7 +12,7 @@ async fn test_rewrite_system_peers(connection: &CassandraConnection) {
         ResultValue::Inet("255.255.255.255".into()),
         // rack is non-determistic because we dont know which node this will be
         ResultValue::Any,
-        ResultValue::Varchar("3.11.13".into()),
+        ResultValue::Varchar("4.0.6".into()),
         ResultValue::Inet("255.255.255.255".into()),
         // schema_version is non deterministic so we cant assert on it.
         ResultValue::Any,
@@ -26,7 +26,7 @@ async fn test_rewrite_system_peers(connection: &CassandraConnection) {
         ResultValue::Any,
         ResultValue::Inet("255.255.255.255".into()),
         ResultValue::Any,
-        ResultValue::Varchar("3.11.13".into()),
+        ResultValue::Varchar("4.0.6".into()),
         ResultValue::Inet("255.255.255.255".into()),
         // schema_version is non deterministic so we cant assert on it.
         ResultValue::Any,
@@ -59,40 +59,89 @@ async fn test_rewrite_system_peers(connection: &CassandraConnection) {
     .await;
 }
 
+async fn test_rewrite_system_peers_v2(connection: &CassandraConnection) {
+    let star_results = [
+        // peer is non-determistic because we dont know which node this will be
+        ResultValue::Any,
+        ResultValue::Int(7000),
+        ResultValue::Varchar("dc1".into()),
+        // host_id is non-determistic because we dont know which node this will be
+        ResultValue::Any,
+        // native_address is non-determistic because we dont know which node this will be
+        ResultValue::Any,
+        ResultValue::Int(9042),
+        ResultValue::Inet("255.255.255.255".into()),
+        ResultValue::Int(-1),
+        // rack is non-determistic because we dont know which node this will be
+        ResultValue::Any,
+        ResultValue::Varchar("4.0.6".into()),
+        // schema_version is non deterministic so we cant assert on it.
+        ResultValue::Any,
+        // Unfortunately token generation appears to be non-deterministic but we can at least assert that
+        // there are 128 tokens per node
+        ResultValue::Set(std::iter::repeat(ResultValue::Any).take(128).collect()),
+    ];
+
+    let all_columns = "peer, peer_port, data_center, host_id, native_address, native_port, preferred_ip, preferred_port, rack, release_version, schema_version, tokens";
+    assert_query_result(
+        connection,
+        "SELECT * FROM system.peers_v2;",
+        &[&star_results, &star_results],
+    )
+    .await;
+    assert_query_result(
+        connection,
+        &format!("SELECT {all_columns} FROM system.peers_v2;"),
+        &[&star_results, &star_results],
+    )
+    .await;
+    assert_query_result(
+        connection,
+        &format!("SELECT {all_columns}, {all_columns} FROM system.peers_v2;"),
+        &[
+            &[star_results.as_slice(), star_results.as_slice()].concat(),
+            &[star_results.as_slice(), star_results.as_slice()].concat(),
+        ],
+    )
+    .await;
+}
+
 async fn test_rewrite_system_local(connection: &CassandraConnection) {
     let star_results = [
         ResultValue::Varchar("local".into()),
         ResultValue::Varchar("COMPLETED".into()),
         // broadcast address is non-deterministic because we dont know which node this will be
         ResultValue::Any,
+        ResultValue::Int(7000),
         ResultValue::Varchar("TestCluster".into()),
-        ResultValue::Varchar("3.4.4".into()),
+        ResultValue::Varchar("3.4.5".into()),
         ResultValue::Varchar("dc1".into()),
         // gossip_generation is non deterministic cant assert on it
         ResultValue::Any,
         // host_id is non-deterministic because we dont know which node this will be
         ResultValue::Any,
         ResultValue::Inet("127.0.0.1".parse().unwrap()),
-        ResultValue::Varchar("4".into()),
+        ResultValue::Int(7000),
+        ResultValue::Varchar("5".into()),
         ResultValue::Varchar("org.apache.cassandra.dht.Murmur3Partitioner".into()),
         // rack is non-deterministic because we dont know which node this will be
         ResultValue::Any,
-        ResultValue::Varchar("3.11.13".into()),
+        ResultValue::Varchar("4.0.6".into()),
         ResultValue::Inet("0.0.0.0".parse().unwrap()),
+        ResultValue::Int(9042),
         // schema_version is non deterministic so we cant assert on it.
-        ResultValue::Any,
-        // thrift_version isnt used anymore so I dont really care what it maps to
         ResultValue::Any,
         // Unfortunately token generation appears to be non-deterministic but we can at least assert that
         // there are 128 tokens per node
         ResultValue::Set(std::iter::repeat(ResultValue::Any).take(128).collect()),
-        ResultValue::Map(vec![]),
+        // truncated_at is non deterministic so we cant assert on it.
+        ResultValue::Any,
     ];
 
     let all_columns =
-        "key, bootstrapped, broadcast_address, cluster_name, cql_version, data_center,
-        gossip_generation, host_id, listen_address, native_protocol_version, partitioner, rack,
-        release_version, rpc_address, schema_version, thrift_version, tokens, truncated_at";
+        "key, bootstrapped, broadcast_address, broadcast_port, cluster_name, cql_version, data_center,
+        gossip_generation, host_id, listen_address, listen_port, native_protocol_version, partitioner, rack,
+        release_version, rpc_address, rpc_port, schema_version, tokens, truncated_at";
 
     assert_query_result(connection, "SELECT * FROM system.local;", &[&star_results]).await;
     assert_query_result(
@@ -112,6 +161,7 @@ async fn test_rewrite_system_local(connection: &CassandraConnection) {
 pub async fn test(connection: &CassandraConnection) {
     test_rewrite_system_local(connection).await;
     test_rewrite_system_peers(connection).await;
+    test_rewrite_system_peers_v2(connection).await;
 }
 
 pub async fn test_topology_task(ca_path: Option<&str>) {
