d3eb22d491f08569d712aaa078e2655690530dbb
Lucas Kent <rubickent@gmail.com>
2023-01-31 10:37:11 +1100

    ShotoverManager -> ShotoverProcess in transform int tests (#997)

--- test-helpers/src/bench.rs
+++ test-helpers/src/bench.rs
@@ -6,7 +6,7 @@ pub fn init() {
     }
 
     // The benches and tests automatically set the working directory to CARGO_MANIFEST_DIR.
-    // We need to do the same as the DockerCompose + ShotoverManager types rely on this.
+    // We need to do the same as the DockerCompose + ShotoverProcess types rely on this.
     std::env::set_current_dir(
         Path::new(env!("CARGO_MANIFEST_DIR"))
             .parent()
