18ce0d7eaeb8430247f4f70f601f4125a35f35b4
Lucas Kent <rubickent@gmail.com>
2023-01-30 15:37:08 +1100

    Remove ShotoverManager

--- shotover-proxy/tests/runner/runner_int_tests.rs
+++ shotover-proxy/tests/runner/runner_int_tests.rs
@@ -1,34 +1,8 @@
-use crate::helpers::ShotoverManager;
 use serial_test::serial;
-use std::any::Any;
 use test_helpers::shotover_process::{
     shotover_from_topology_file, shotover_from_topology_file_fail_to_startup, Count, EventMatcher,
     Level,
 };
-
-#[tokio::test(flavor = "multi_thread")]
-#[serial]
-async fn test_runtime_use_existing() {
-    let shotover_manager =
-        ShotoverManager::from_topology_file("example-configs/null-redis/topology.yaml");
-
-    // Assert that shotover is using the test runtime
-    let handle = tokio::runtime::Handle::current();
-    assert_eq!(handle.type_id(), shotover_manager.runtime_handle.type_id());
-
-    // Assert that shotover did not create a runtime for itself
-    assert!(shotover_manager.runtime.is_none());
-}
-
-#[test]
-#[serial]
-fn test_runtime_create() {
-    let shotover_manager =
-        ShotoverManager::from_topology_file("example-configs/null-redis/topology.yaml");
-
-    // Assert that shotover created a runtime for itself
-    assert!(shotover_manager.runtime.is_some());
-}
 
 #[tokio::test]
 #[serial]
