b0164e6f5fce14c249ae0bd3620b77ce0036474f
Lucas Kent <rubickent@gmail.com>
2023-01-31 15:44:45 +1100

    prevent recompiling by properly specifying build.rs

--- tokio-bin-process/build.rs
+++ tokio-bin-process/build.rs
@@ -3,4 +3,5 @@ use std::env;
 fn main() {
     let profile = env::var("PROFILE").unwrap();
     println!("cargo:rustc-env=PROFILE={profile}");
+    println!("cargo:rerun-if-env-changed=PROFILE");
 }
