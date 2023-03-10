084be3c079fbdb1d0da4dac2fb6fddf8d9e7f4a3
Lucas Kent <rubickent@gmail.com>
2023-03-10 14:47:08 +1100

    Add ShotoverProcessBuilder::with_observability_address (#1073)

--- test-helpers/src/shotover_process.rs
+++ test-helpers/src/shotover_process.rs
@@ -1,4 +1,5 @@
 use std::time::Duration;
+use uuid::Uuid;
 
 pub use tokio_bin_process::event::{Event, Level};
 pub use tokio_bin_process::event_matcher::{Count, EventMatcher, Events};
@@ -8,6 +9,7 @@ pub struct ShotoverProcessBuilder {
     topology_path: String,
     log_name: Option<String>,
     cores: Option<String>,
+    observability_port: Option<u16>,
 }
 
 impl ShotoverProcessBuilder {
@@ -16,6 +18,7 @@ impl ShotoverProcessBuilder {
             topology_path: topology_path.to_owned(),
             log_name: None,
             cores: None,
+            observability_port: None,
         }
     }
 
@@ -29,11 +32,32 @@ impl ShotoverProcessBuilder {
         self
     }
 
+    pub fn with_observability_port(mut self, port: u16) -> Self {
+        self.observability_port = Some(port);
+        self
+    }
+
     pub async fn start(&self) -> BinProcess {
         let mut args = vec!["-t", &self.topology_path, "--log-format", "json"];
         if let Some(cores) = &self.cores {
             args.extend(["--core-threads", cores]);
         }
+        let config_path = if let Some(observability_port) = self.observability_port {
+            let config_path = std::env::temp_dir().join(Uuid::new_v4().to_string());
+            let config_contents = format!(
+                r#"
+---
+main_log_level: "info,shotover_proxy=info"
+observability_interface: "127.0.0.1:{observability_port}"
+"#
+            );
+            std::fs::write(&config_path, config_contents).unwrap();
+            config_path.into_os_string().into_string().unwrap()
+        } else {
+            "config/config.yaml".to_owned()
+        };
+        args.extend(["-c", &config_path]);
+
         let mut shotover = BinProcess::start_with_args(
             "shotover-proxy",
             &args,
