424fdd35a5daaf9d1ef6c3db3bcc81e8f7e881fb
Lucas Kent <rubickent@gmail.com>
2023-04-03 17:56:13 +1000

    use new shotover API

--- custom-transforms-example/src/main.rs
+++ custom-transforms-example/src/main.rs
@@ -1,18 +1,7 @@
-use anyhow::Result;
-use clap::Parser;
-
-use shotover::runner::{ConfigOpts, Runner};
+use shotover::runner::Shotover;
 
 mod redis_get_rewrite;
 
-fn main() -> Result<()> {
-    // TODO: this API should become just `Runner::new().run_block()`
-    // * User shouldnt have to provide ConfigOpts
-    // * The user shouldnt have to manually set `with_observability_interface`
-    // * Runner::new() should internally perform the backup tracing reporter logic that is currently in `shotover-proxy/src/main.rs`
-    // * run_block should also return `!`
-    // * Using a basic builder gives us room to non-breakingly expand the API in the future
-    Runner::new(ConfigOpts::parse())?
-        .with_observability_interface()?
-        .run_block()
+fn main() {
+    Shotover::new().run_block();
 }
