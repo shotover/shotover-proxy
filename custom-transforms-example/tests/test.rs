ca140f15851740a21cb412be46087a73b5bf9377
Lucas Kent <rubickent@gmail.com>
2023-04-11 08:52:32 +1000

    Allow for custom transforms via using shotover as a crate (#1101)

---
+++ custom-transforms-example/tests/test.rs
@@ -0,0 +1,66 @@
+use redis::aio::Connection;
+use redis::Cmd;
+use std::time::Duration;
+use test_helpers::connection::redis_connection;
+use test_helpers::docker_compose::DockerCompose;
+use test_helpers::shotover_process::{BinProcess, EventMatcher, Level};
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_custom_transform() {
+    // Setup shotover and the redis server it connects to
+    let _compose = DockerCompose::new("config/docker-compose.yaml");
+    let shotover = shotover_proxy("config/topology.yaml").await;
+    let mut connection = redis_connection::new_async(6379).await;
+
+    // Verify functionality of transform
+    assert_ok(
+        redis::cmd("SET").arg("foo").arg("some value"),
+        &mut connection,
+    )
+    .await;
+    assert_bytes(
+        redis::cmd("GET").arg("foo"),
+        &mut connection,
+        b"Rewritten value",
+    )
+    .await;
+    assert_bytes(
+        redis::cmd("GET").arg("bar"),
+        &mut connection,
+        b"Rewritten value",
+    )
+    .await;
+
+    // Shutdown shotover asserting that it encountered no errors
+    shotover.shutdown_and_then_consume_events(&[]).await;
+}
+
+async fn shotover_proxy(topology_path: &str) -> BinProcess {
+    let mut shotover = BinProcess::start_with_args(
+        "custom-transforms-example",
+        "shotover",
+        &["-t", topology_path, "--log-format", "json"],
+    )
+    .await;
+
+    tokio::time::timeout(
+        Duration::from_secs(30),
+        shotover.wait_for(
+            &EventMatcher::new()
+                .with_level(Level::Info)
+                .with_target("shotover::server")
+                .with_message("accepting inbound connections"),
+        ),
+    )
+    .await
+    .unwrap();
+    shotover
+}
+
+pub async fn assert_ok(cmd: &mut Cmd, connection: &mut Connection) {
+    assert_eq!(cmd.query_async(connection).await, Ok("OK".to_string()));
+}
+
+pub async fn assert_bytes(cmd: &mut Cmd, connection: &mut Connection, value: &[u8]) {
+    assert_eq!(cmd.query_async(connection).await, Ok(value.to_vec()));
+}
