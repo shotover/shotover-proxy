86c0907226bf5874292cbeff5d165cd52bedefd0
Lucas Kent <rubickent@gmail.com>
2023-05-30 10:25:33 +1000

    windsock: Add tracing logger to our main

--- shotover-proxy/examples/windsock/main.rs
+++ shotover-proxy/examples/windsock/main.rs
@@ -4,9 +4,16 @@ mod kafka;
 use cassandra::*;
 use kafka::*;
 use std::path::Path;
+use tracing_subscriber::EnvFilter;
 use windsock::Windsock;
 
 fn main() {
+    let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(non_blocking)
+        .init();
+
     // The benches and tests automatically set the working directory to CARGO_MANIFEST_DIR.
     // We need to do the same as the DockerCompose + ShotoverProcess types rely on this.
     std::env::set_current_dir(
