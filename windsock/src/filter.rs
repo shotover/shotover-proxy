8174329f2c39a7f13684ac2bf6272a03fc273983
Lucas Kent <rubickent@gmail.com>
2023-06-06 14:39:45 +1000

    windsock: filter: match any of multiple values (#1207)

--- windsock/src/filter.rs
+++ windsock/src/filter.rs
@@ -1,8 +1,13 @@
 use crate::bench::Tags;
 use anyhow::{anyhow, Result};
 
+struct FilterTag {
+    key: String,
+    values: Vec<String>,
+}
+
 pub(crate) struct Filter {
-    filter: Vec<[String; 2]>,
+    filter: Vec<FilterTag>,
 }
 
 impl Filter {
@@ -10,9 +15,9 @@ impl Filter {
         let mut filter = vec![];
         for pair in query.split_whitespace() {
             let mut iter = pair.split('=');
-            let key = iter.next().unwrap();
-            let value = match iter.next() {
-                Some(value) => value,
+            let key = iter.next().unwrap().to_owned();
+            let values = match iter.next() {
+                Some(rhs) => rhs.split('|').map(|x| x.to_owned()).collect(),
                 None => {
                     return Err(anyhow!(
                         "Expected exactly one '=' but found no '=' in tag {pair:?}"
@@ -24,16 +29,16 @@ impl Filter {
                     "Expected exactly one '=' but found multiple '=' in tag {pair:?}"
                 ));
             }
-            filter.push([key.to_owned(), value.to_owned()])
+            filter.push(FilterTag { key, values })
         }
         Ok(Filter { filter })
     }
 
     pub(crate) fn matches(&self, tags: &Tags) -> bool {
-        for [key, value] in &self.filter {
+        for FilterTag { key, values } in &self.filter {
             match tags.0.get(key) {
-                Some(other_value) => {
-                    if value != other_value {
+                Some(check_value) => {
+                    if !values.contains(check_value) {
                         return false;
                     }
                 }
