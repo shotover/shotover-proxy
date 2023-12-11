9ab82f7f6ddabd8c91566ca9cf1886bf5ea472ca
Lucas Kent <rubickent@gmail.com>
2023-12-12 09:44:27 +1100

    Fix shotover release process (#1389)

--- shotover-proxy/build/build_release.sh
+++ shotover-proxy/build/build_release.sh
@@ -4,28 +4,17 @@ set -e
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $SCRIPT_DIR/../..
+cd $SCRIPT_DIR
 
-cargo test --release
 cargo build --release
 
-# Make glibc version
 mkdir -p shotover
-cp target/release/shotover-proxy shotover
-cp -r shotover-proxy/config shotover
+cp ../../target/release/shotover-proxy shotover
+cp -r ../config shotover
 
 # extract the crate version from Cargo.toml
 CRATE_VERSION="$(cargo metadata --format-version 1 --offline --no-deps | jq -c -M -r '.packages[] | select(.name == "shotover-proxy") | .version')"
-tar -cvzf shotover-proxy-linux_amd64-${CRATE_VERSION}.tar.gz shotover
+tar -cvzf out.tar.gz shotover
+mv out.tar.gz shotover-proxy-linux_amd64-${CRATE_VERSION}.tar.gz
 
 rm -rf shotover
-
-
-### Building musl with rust-musl-builder only works on older rust toolchain versions so this is commented out until we can reenable it.
-# Make musl version
-#$SCRIPT_DIR/build_static.sh
-# mkdir -p shotover-proxy
-# cp target/x86_64-unknown-linux-musl/release/shotover-proxy shotover-proxy
-# cp -r config shotover-proxy
-# tar -cvzf shotover-proxy-static_linux_amd64-"$(cargo metadata --format-version 1 --offline --no-deps | jq -c -M -r '.packages[0].version')".tar.gz shotover-proxy
-# rm -rf shotover-proxy
