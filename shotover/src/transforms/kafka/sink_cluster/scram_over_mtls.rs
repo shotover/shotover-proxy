255b796b2f5d401f732c902dc868f16cd75d4131
Justin Weng <justin.weng@netapp.com>
2024-07-11 10:19:12 +1000

    Add comment on not being able to expire other users' delegation tokens  (#1686)

--- shotover/src/transforms/kafka/sink_cluster/scram_over_mtls.rs
+++ shotover/src/transforms/kafka/sink_cluster/scram_over_mtls.rs
@@ -123,6 +123,10 @@ async fn task(
                 let passed = instant.elapsed();
                 tracing::info!("Delegation token for {username:?} recreated in {passed:?}");
                 token_creation_time_metric.record(passed);
+
+                // TODO: We would expire the old token here if it were possible, but it is not since kafka will not allow users (even super users) to expire a token belonging to another user.
+                // See details in https://github.com/shotover/shotover-proxy/pull/1685
+                // However, at this point, the token will automatically expire itself in delegation_token_lifetime / 2, so it is not a huge concern.
             }
             result = rx.recv() => {
                 if let Some(request) = result {
