d7547741d8b10c5f64f133e1145bf843f7fb57ec
Lucas Kent <rubickent@gmail.com>
2024-12-05 16:47:30 +1100

    Fix binary release (#1858)

--- shotover-proxy/build/build_release.sh
+++ shotover-proxy/build/build_release.sh
@@ -15,6 +15,6 @@ cp -r ../config shotover
 # extract the crate version from Cargo.toml
 CRATE_VERSION="$(cargo metadata --format-version 1 --offline --no-deps | jq -c -M -r '.packages[] | select(.name == "shotover-proxy") | .version')"
 tar -cvzf out.tar.gz shotover
-mv out.tar.gz shotover-proxy-linux_amd64-${CRATE_VERSION}.tar.gz
+mv out.tar.gz ../../shotover-proxy-linux_amd64-${CRATE_VERSION}.tar.gz
 
 rm -rf shotover
