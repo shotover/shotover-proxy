0c531fdf7fe4395c193277a9bd878fe35bd4cc96
Tan Nguyen <tan.nguyen@netapp.com>
2025-04-08 09:46:53 +0700

    test: lucas approach

--- shotover-proxy/benches/windsock/main.rs
+++ shotover-proxy/benches/windsock/main.rs
@@ -21,8 +21,8 @@ mod valkey;
 
 use cloud::CloudResources;
 use cloud::CloudResourcesRequired;
-use fred::rustls::crypto::aws_lc_rs::default_provider;
 use std::path::Path;
+use fred::rustls::crypto::ring::default_provider;
 use tracing_subscriber::EnvFilter;
 use windsock::{Bench, Windsock};
 
@@ -31,7 +31,7 @@ pub type ShotoverBench = Box<
 >;
 
 fn main() {
-    // default_provider().install_default().unwrap();
+    default_provider().install_default().ok();
 
     let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
     tracing_subscriber::fmt()
