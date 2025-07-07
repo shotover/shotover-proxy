7ea8e9c53ee6f38baa7ab2f2866c3289337ae08f
Richu Promod <richupromod@gmail.com>
2025-07-07 16:10:05 +1000

    review comments resolved 2.O

--- shotover-proxy/tests/runner/hotreload_int_tests.rs
+++ shotover-proxy/tests/runner/hotreload_int_tests.rs
@@ -2,7 +2,7 @@ use crate::shotover_process;
 #[tokio::test]
 async fn test_hotreload_cli_flag_accepted() {
     let shotover_process = shotover_process("tests/test-configs/null-valkey/topology.yaml")
-        .with_hotreload(true) // Use the new method we'll create
+        .with_hotreload(true)
         .with_config("tests/test-configs/shotover-config/config_metrics_disabled.yaml")
         .start()
         .await;
