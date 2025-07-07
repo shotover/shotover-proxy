9301e017593c38241005116775ba9273c7844975
Richu Promod <richupromod@gmail.com>
2025-07-07 11:50:43 +1000

    hotreload interface

---
+++ shotover-proxy/tests/runner/hotreload_int_tests.rs
@@ -0,0 +1,35 @@
+use crate::shotover_process;
+use test_helpers::shotover_process::{EventMatcher, Level};
+#[tokio::test]
+async fn test_hotreload_cli_flag_works() {
+    let shotover_process = shotover_process(
+        "tests/test-configs/null-valkey/
+topology.yaml",
+    )
+    .with_args(&["--hotreload"])
+    .with_config(
+        "tests/test-configs/shotover-config/
+config_metrics_disabled.yaml",
+    )
+    .start()
+    .await;
+    // log messages
+    let events = shotover_process
+        .shutdown_and_then_consume_events(&[EventMatcher::new()
+            .with_level(Level::Info)
+            .with_target("shotover::runner")
+            .with_message(
+                "Hot reloading is ENABLED - shotover will support hot
+reload operations",
+            )])
+        .await;
+    events.assert_contains(
+        &EventMatcher::new()
+            .with_level(Level::Info)
+            .with_target("shotover::runner")
+            .with_message(
+                "Hot reloading is ENABLED - shotover will support hot reload
+operations",
+            ),
+    );
+}
--- shotover-proxy/tests/runner/mod.rs
+++ shotover-proxy/tests/runner/mod.rs
@@ -1,2 +1,3 @@
+mod hotreload_int_tests;
 mod observability_int_tests;
 mod runner_int_tests;
