bf706310e738ee5c281adb7673e6903595475c9e
Lucas Kent <rubickent@gmail.com>
2026-03-11 09:54:34 +1100

    ec2-cargo-fixes (#1987)

--- ec2-cargo/src/main.rs
+++ ec2-cargo/src/main.rs
@@ -1,34 +1,41 @@
+mod cli;
+mod instance;
+
 use aws_throwaway::{
     Aws, CleanupResources, Ec2Instance, Ec2InstanceDefinition, IngressRestriction, InstanceType,
 };
 use cargo_metadata::{Metadata, MetadataCommand};
 use clap::Parser;
-use shellfish::{Command, Shell, async_fn, handler::DefaultAsyncHandler, rustyline::DefaultEditor};
-use std::error::Error;
-use std::fs::Permissions;
-use std::os::unix::prelude::PermissionsExt;
+use cli::{Args, Cmd};
+use instance::Instance;
+use std::path::PathBuf;
 use tracing_subscriber::EnvFilter;
 
-/// Spins up an EC2 instance and then presents a shell from which you can run `cargo test` on the ec2 instance.
-///
-/// Every time the shell runs a cargo command, any local changes to the repo are reuploaded to the ec2 instance.
-///
-/// When the shell is exited all created EC2 instances are destroyed.
-#[derive(Parser, Clone)]
-#[clap()]
-pub struct Args {
-    /// Specify the instance type to use for building and running tests.
-    #[clap(long, default_value = "c7g.2xlarge")]
-    pub instance_type: String,
+fn state_file_path(cargo_meta: &Metadata) -> PathBuf {
+    cargo_meta
+        .target_directory
+        .join("ec2-cargo-state.json")
+        .into()
+}
 
-    /// Cleanup resources and then immediately terminate without opening the shell
-    #[clap(long)]
-    pub cleanup: bool,
+async fn load_instance(cargo_meta: &Metadata) -> Instance {
+    let path = state_file_path(cargo_meta);
+    let data = std::fs::read_to_string(&path).unwrap_or_else(|_| {
+        panic!(
+            "No instance state found at {}. Run `create` first.",
+            path.display()
+        )
+    });
+    let mut ec2: Ec2Instance =
+        serde_json::from_str(&data).expect("Failed to deserialize instance state");
+    ec2.init()
+        .await
+        .expect("Failed to initialize SSH connection to instance");
+    Instance(ec2)
 }
 
 #[tokio::main]
 async fn main() {
-    // Needed to disable anyhow stacktraces by default
     if std::env::var("RUST_LIB_BACKTRACE").is_err() {
         std::env::set_var("RUST_LIB_BACKTRACE", "0");
     }
@@ -41,30 +48,38 @@ async fn main() {
 
     let cargo_meta = MetadataCommand::new().exec().unwrap();
     let args = Args::parse();
-    if args.cleanup {
-        Aws::cleanup_resources_static(CleanupResources::AllResources).await;
-        println!("All AWS throwaway resources have been deleted");
-        return;
-    }
 
+    match args.command {
+        Cmd::Create { instance_type } => create(cargo_meta, &instance_type).await,
+        Cmd::Test { args } => test(cargo_meta, &args).await,
+        Cmd::Windsock { args } => windsock("windsock", cargo_meta, &args).await,
+        Cmd::WindsockKafka { args } => windsock("windsock-kafka", cargo_meta, &args).await,
+        Cmd::WindsockCassandra { args } => windsock("windsock-cassandra", cargo_meta, &args).await,
+        Cmd::WindsockValkey { args } => windsock("windsock-valkey", cargo_meta, &args).await,
+        Cmd::SshInstructions => ssh_instructions(cargo_meta).await,
+        Cmd::Cleanup => cleanup(cargo_meta).await,
+    }
+}
+
+async fn create(cargo_meta: Metadata, instance_type: &str) {
     let aws = Aws::builder(CleanupResources::AllResources)
         .use_az(Some("us-east-1b".into()))
         .use_ingress_restriction(IngressRestriction::LocalPublicAddress)
         .build()
         .await;
-    let instance_type = InstanceType::from(args.instance_type.as_str());
+    let instance_type = InstanceType::from(instance_type);
     let instance = aws
         .create_ec2_instance(Ec2InstanceDefinition::new(instance_type).volume_size_gigabytes(40))
         .await;
 
+    let instance = Instance(instance);
+
     println!(
         "If something goes wrong with setup you can ssh into the machine by:\n{}",
-        instance.ssh_instructions()
+        instance.0.ssh_instructions()
     );
 
-    let mut receiver = instance
-        .ssh()
-        .shell_stdout_lines(r#"
+    instance.run_command(r#"
 set -e
 set -u
 # Need to retry until succeeds as apt-get update may fail if run really quickly after starting the instance
@@ -72,7 +87,7 @@ until sudo apt-get update -qq
 do
   sleep 1
 done
-sudo apt-get install -y cmake pkg-config g++ libssl-dev librdkafka-dev unzip default-jre-headless npm
+sudo apt-get install -y cmake pkg-config g++ libssl-dev librdkafka-dev unzip default-jre-headless npm libcurl4-openssl-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 curl -sSL https://get.docker.com/ | sudo sh
@@ -102,245 +117,81 @@ else
     unzip -q awscliv2.zip
     sudo ./aws/install
 fi
-"#).await;
-    while let Some(line) = receiver.recv().await {
-        match line {
-            Ok(line) => println!("{line}"),
-            Err(err) => panic!("Failed to setup instance: {err:?}"),
-        }
-    }
+"#).await.expect("Failed to setup instance");
 
     println!("Finished creating instance.");
 
-    let access_key_id = std::env::var("AWS_ACCESS_KEY_ID").unwrap();
-    let secret_access_key = std::env::var("AWS_SECRET_ACCESS_KEY").unwrap();
-
-    let mut shell = Shell::new_with_async_handler(
-        State {
-            cargo_meta,
-            instance,
-            access_key_id,
-            secret_access_key,
-        },
-        "ec2-cargo$ ",
-        DefaultAsyncHandler::default(),
-        DefaultEditor::new().unwrap(),
-    );
-    shell.commands.insert(
-        "test",
-        Command::new_async(
-            "Uploads changes and runs `cargo nextest run $args`".to_owned(),
-            async_fn!(State, test),
-        ),
-    );
-    shell.commands.insert(
-        "windsock",
-        Command::new_async(
-            "Uploads changes and runs `cargo windsock $args`. Windsock results are downloaded to target/windsock_data".to_owned(),
-            async_fn!(State, windsock),
-        ),
-    );
-    shell.commands.insert(
-        "windsock-kafka",
-        Command::new_async(
-            "Uploads changes and runs `cargo windsock-kafka $args`. Windsock results are downloaded to target/windsock_data".to_owned(),
-            async_fn!(State, windsock_kafka),
-        ),
-    );
-    shell.commands.insert(
-        "windsock-cassandra",
-        Command::new_async(
-            "Uploads changes and runs `cargo windsock-cassandra $args`. Windsock results are downloaded to target/windsock_data".to_owned(),
-            async_fn!(State, windsock_cassandra),
-        ),
-    );
-    shell.commands.insert(
-        "windsock-valkey",
-        Command::new_async(
-            "Uploads changes and runs `cargo windsock-valkey $args`. Windsock results are downloaded to target/windsock_data".to_owned(),
-            async_fn!(State, windsock_valkey),
-        ),
-    );
-    shell.commands.insert(
-        "ssh-instructions",
-        Command::new(
-            "Print a bash snippet that can be used to ssh into the machine".to_owned(),
-            ssh_instructions,
-        ),
-    );
-
-    shell.run_async().await.unwrap();
-
-    aws.cleanup_resources().await;
-    println!("All AWS throwaway resources have been deleted")
+    let path = state_file_path(&cargo_meta);
+    let data = serde_json::to_string(&instance.0).unwrap();
+    std::fs::write(&path, data).unwrap();
+    println!("Instance state saved to {}", path.display());
 }
 
-async fn test(state: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    rsync_push_shotover(state).await;
-    let args = process_args(args);
-    let mut receiver = state
-        .instance
-        .ssh()
-        .shell_stdout_lines(&format!(
+async fn test(cargo_meta: Metadata, args: &[String]) {
+    let instance = load_instance(&cargo_meta).await;
+    println!("Uploading shotover project");
+    instance.rsync_push_shotover(&cargo_meta).await;
+    println!("Upload complete");
+    let args = quote_args(args);
+    if let Err(err) = instance
+        .run_command(&format!(
             r#"
 source .profile
 cd shotover
-cargo nextest run {} 2>&1
-"#,
-            args
+cargo nextest run {args} 2>&1
+"#
         ))
-        .await;
-    while let Some(line) = receiver.recv().await {
-        match line {
-            Ok(line) => println!("{line}"),
-            Err(err) => println!("{err:?}"),
-        }
+        .await
+    {
+        println!("{err:?}");
     }
-
-    Ok(())
 }
 
-async fn windsock(state: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    windsock_inner("windsock", state, args).await
-}
+async fn windsock(command: &str, cargo_meta: Metadata, args: &[String]) {
+    let instance = load_instance(&cargo_meta).await;
+    let access_key_id = std::env::var("AWS_ACCESS_KEY_ID").unwrap();
+    let secret_access_key = std::env::var("AWS_SECRET_ACCESS_KEY").unwrap();
 
-async fn windsock_kafka(state: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    windsock_inner("windsock-kafka", state, args).await
-}
-
-async fn windsock_cassandra(state: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    windsock_inner("windsock-cassandra", state, args).await
-}
-
-async fn windsock_valkey(state: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    windsock_inner("windsock-valkey", state, args).await
-}
-
-async fn windsock_inner(
-    command: &str,
-    state: &mut State,
-    args: Vec<String>,
-) -> Result<(), Box<dyn Error>> {
-    rsync_push_shotover(state).await;
-    let args = process_args(args);
-    let mut receiver = state
-        .instance
-        .ssh()
-        .shell_stdout_lines(&format!(
+    println!("Uploading shotover project");
+    instance.rsync_push_shotover(&cargo_meta).await;
+    println!("Upload complete");
+    let args = quote_args(args);
+    if let Err(err) = instance
+        .run_command(&format!(
             r#"
 source .profile
 cd shotover
 # ensure windsock_data exists so that fetching results still succeeds even if windsock never created the directory
 mkdir -p target/windsock_data
-AWS_ACCESS_KEY_ID={} AWS_SECRET_ACCESS_KEY={} cargo {command} {args} 2>&1
-"#,
-            state.access_key_id,
-            state.secret_access_key
+RUST_LOG=info AWS_ACCESS_KEY_ID={access_key_id} AWS_SECRET_ACCESS_KEY={secret_access_key} cargo {command} {args} 2>&1
+"#
         ))
-        .await;
-    while let Some(line) = receiver.recv().await {
-        match line {
-            Ok(line) => println!("{line}"),
-            Err(err) => println!("{err:?}"),
-        }
+        .await
+    {
+        println!("{err:?}");
     }
 
-    rsync_fetch_windsock_results(state).await;
-
-    Ok(())
+    instance.rsync_fetch_windsock_results(&cargo_meta).await;
 }
 
-fn process_args(mut args: Vec<String>) -> String {
-    args.remove(0);
+async fn ssh_instructions(cargo_meta: Metadata) {
+    let instance = load_instance(&cargo_meta).await;
+    println!(
+        "Run the following to ssh into the EC2 instance:\n{}",
+        instance.0.ssh_instructions()
+    );
+}
+
+async fn cleanup(cargo_meta: Metadata) {
+    Aws::cleanup_resources_static(CleanupResources::AllResources).await;
+    let path = state_file_path(&cargo_meta);
+    let _ = std::fs::remove_file(&path);
+    println!("All AWS throwaway resources have been deleted");
+}
+
+fn quote_args(args: &[String]) -> String {
     args.iter()
         .map(|x| String::from_utf8(shell_quote::Bash::quote_vec(x)).unwrap())
         .collect::<Vec<_>>()
         .join(" ")
-}
-
-async fn rsync_push_shotover(state: &State) {
-    let instance = &state.instance;
-    let project_root_dir = &state.cargo_meta.workspace_root;
-    let address = instance.public_ip().unwrap().to_string();
-
-    rsync(
-        state,
-        vec![
-            "--exclude".to_owned(),
-            "target".to_owned(),
-            format!("{}/", project_root_dir), // trailing slash means copy the contents of the directory instead of the directory itself
-            format!("ubuntu@{address}:/home/ubuntu/shotover"),
-        ],
-    )
-    .await
-}
-
-async fn rsync_fetch_windsock_results(state: &State) {
-    let instance = &state.instance;
-    let windsock_dir = &state.cargo_meta.target_directory.join("windsock_data");
-    let address = instance.public_ip().unwrap().to_string();
-
-    rsync(
-        state,
-        vec![
-            format!("ubuntu@{address}:/home/ubuntu/shotover/target/windsock_data/"), // trailing slash means copy the contents of the directory instead of the directory itself
-            windsock_dir.to_string(),
-        ],
-    )
-    .await
-}
-
-async fn rsync(state: &State, append_args: Vec<String>) {
-    let instance = &state.instance;
-    let target_dir = &state.cargo_meta.target_directory;
-
-    let key_path = target_dir.join("ec2-cargo-privatekey");
-    tokio::fs::remove_file(&key_path).await.ok();
-    tokio::fs::write(&key_path, instance.client_private_key())
-        .await
-        .unwrap();
-    std::fs::set_permissions(&key_path, Permissions::from_mode(0o400)).unwrap();
-
-    let known_hosts_path = target_dir.join("ec2-cargo-known_hosts");
-    tokio::fs::write(&known_hosts_path, instance.openssh_known_hosts_line())
-        .await
-        .unwrap();
-
-    let mut args = vec![
-        "--delete".to_owned(),
-        "-e".to_owned(),
-        format!(
-            "ssh -i {} -o 'UserKnownHostsFile {}'",
-            key_path, known_hosts_path
-        ),
-        "-ra".to_owned(),
-    ];
-    args.extend(append_args);
-
-    let output = tokio::process::Command::new("rsync")
-        .args(args)
-        .output()
-        .await
-        .unwrap();
-    if !output.status.success() {
-        let stdout = String::from_utf8(output.stdout).unwrap();
-        let stderr = String::from_utf8(output.stderr).unwrap();
-        panic!("rsync failed:\nstdout:\n{stdout}\nstderr:\n{stderr}")
-    }
-}
-
-fn ssh_instructions(state: &mut State, mut _args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    println!(
-        "Run the following to ssh into the EC2 instance:\n{}",
-        state.instance.ssh_instructions()
-    );
-
-    Ok(())
-}
-
-struct State {
-    cargo_meta: Metadata,
-    instance: Ec2Instance,
-    access_key_id: String,
-    secret_access_key: String,
 }
